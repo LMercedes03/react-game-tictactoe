bef8b36d01d77df640ef0752319445f76cf7f7fc
Leonard <lemrcedes030@gmail.com>
2023-05-26 16:12:04 -0400

    Update to react-game-ttt

--- src/Board.js
+++ src/Board.js
@@ -2,10 +2,16 @@ import React from 'react'
 import { Square } from './Square'
 
 export const Board = ({ squares, onClick }) => {
-  const renderSquare = (i) => (
+// Helper function to render each square of game board
+  const renderSquare = (i) => ( 
+    // Renders the Square component with the corresponding value from the squares array and 
+    // attaches the onClick function with the current index i.
     <Square value={squares[i]} onClick={() => onClick(i)} />
-  );
-
+  ); 
+  
+  // Renders three rows of squares using the renderSquare helper function.
+  // Each row consists of three calls to renderSquare with different indices 
+  // (0, 1, 2 for the first row, 3, 4, 5 for the second row, and 6, 7, 8 for the third row).
   return (
     <div className="board">
       <div className="board-row">
@@ -24,5 +30,5 @@ export const Board = ({ squares, onClick }) => {
         {renderSquare(8)}
       </div>
     </div>
-  );
+  )
 };
--- src/Game.js
+++ src/Game.js
@@ -4,50 +4,51 @@ import { CalculateWinner } from "./CalculateWinner"
 import "./TicTacToe.css"
 
 export const Game = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-  const [gameOver, setGameOver] = useState(false);
+  const [squares, setSquares] = useState(Array(9).fill(null));   // State variable to track the state of each square on the board
+  const [xIsNext, setXIsNext] = useState(true);   // State variable to track whose turn it is (X or O)
+  const [gameOver, setGameOver] = useState(false);   // State variable to track if the game is over
 
   const handleClick = (i) => {
-    if (squares[i] || gameOver) return;
+    if (squares[i] || gameOver) return;   // If the square is already filled or the game is over, do nothing
 
-    const newSquares = squares.slice();
-    newSquares[i] = xIsNext ? "X" : "O";
+    const newSquares = squares.slice();   // Create a copy of the squares array to modify it
+    newSquares[i] = xIsNext ? "X" : "O";  // Set the value of the clicked square based on whose turn it is 
 
-    setSquares(newSquares);
-    setXIsNext(!xIsNext);
+    setSquares(newSquares);   // Update the squares state with the modified array
+    setXIsNext(!xIsNext);    // Toggle the turn to the next player
 
     if (CalculateWinner(newSquares) || newSquares.every((square) => square)) {
-      setGameOver(true);
+      // If there is a winner or all squares are filled
+      setGameOver(true);   // Set the game over state to true
     }
   };
 
   const restartGame = () => {
-    setSquares(Array(9).fill(null));
-    setXIsNext(true);
-    setGameOver(false);
+    setSquares(Array(9).fill(null));   // Reset the squares to an array of null values
+    setXIsNext(true);                  // Set the turn back to player X
+    setGameOver(false);                // Reset the game over state to false
   };
 
   const renderStatus = () => {
-    const winner = CalculateWinner(squares);
+    const winner = CalculateWinner(squares);   // Check if there is a winner
     let status;
 
     if (winner) {
-      status = `Winner: ${winner}`;
+      status = `Winner: ${winner}`;   // If there is a winner, set the status message with the winner's symbol
     } else if (squares.every((square) => square)) {
       status = "It's a tie!";
     } else {
-      status = `Next Player: ${xIsNext ? "X" : "O"}`;
+      status = `Next Player: ${xIsNext ? "X" : "O"}`;   // Set the status message with the next player's symbol
     }
 
-    return <div className="status">{status}</div>;
+    return <div className="status">{status}</div>;   // Render the status message 
   };
 
   return (
-    <div className="game">
-      {renderStatus()}
-      <Board squares={squares} onClick={handleClick} />
-      {gameOver && (
+    <div className="game"> 
+      {renderStatus()} 
+      <Board squares={squares} onClick={handleClick} /> 
+      {gameOver && (   // If the game is over, render the restart button
         <button className="restart-button" onClick={restartGame}>
           Restart
         </button>
