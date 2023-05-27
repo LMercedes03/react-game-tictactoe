37f0d2828da2426a033421fd0ec14d3f32df87ad
Leonard <lemrcedes030@gmail.com>
2023-05-27 16:29:10 -0400

    Update to react-game-ttt

--- src/WinnerBanner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const WinnerBanner = ({ winner }) => {
-  return (
-    <div className="winner-banner">
-      {winner === 'Tie' ? 'It\'s a Tie!' : `Winner: ${winner}`}
-    </div>
-  );
-};
